00704f47384e9cf1c09e29f8777a9889fe131955
Shivamdobhal <73071952+Shivamdobhal@users.noreply.github.com>
2022-07-22 22:37:10 +0530

    Create arrays_algorithms.cpp

---
+++ arrays_algorithms.cpp
@@ -0,0 +1,5 @@
+1]  leetcode-53
+    kadanes algorithms-----is a part of dynamic programming and used to solve the problems like max. sum subarray problem.
+                      -----useful when array elements are negative alongwith positive
+                      -----time complexity=o(n)
+                      -----in this algo we discarded the value when it comes negative

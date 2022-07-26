278519330abe3a58fe668d2b6a09cc8babc2aa7f
Shivamdobhal <73071952+Shivamdobhal@users.noreply.github.com>
2022-07-26 11:30:28 +0530

    Update arrays_algorithms.cpp

--- arrays_algorithms.cpp
+++ arrays_algorithms.cpp
@@ -28,3 +28,49 @@ public:
         return max_sum;
     }
 };
+     
+
+
+
+
+
+problem -88       --------two pointer approach
+                 ---------T.C=o(n)  
+                 ---------S.C=0(1)
+                 ---------used to merge two sorted arrays in which start filling the largest  elements from the last
+                 example====  
+                           nums1=1  2  3  0  0  0                  nums2=2  5  6
+                                       p1       i                              p1
+                                       
+                                       
+                               
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>  nums2, int n) {
+        
+        int p1=m-1;
+        int p2=n-1;
+        int i=nums1.size()-1;
+        
+        while(p2>=0)
+        {
+            if( p1>=0 && nums1[p1]>nums2[p2] )             //if p1 is greater
+            {
+                nums1[i]=nums1[p1];
+                i--;
+                p1--;
+            }
+            else                                  
+            {
+                nums1[i]=nums2[p2];                
+                i--;
+                p2--;
+            }
+        }
+      
+    }
+};
+
+
+
+

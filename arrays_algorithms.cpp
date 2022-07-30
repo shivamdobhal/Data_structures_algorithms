48a6bb62f352deb73a9183705fe7999e3c4d9ea7
Shivamdobhal <73071952+Shivamdobhal@users.noreply.github.com>
2022-07-30 22:56:27 +0530

    Update arrays_algorithms.cpp

--- arrays_algorithms.cpp
+++ arrays_algorithms.cpp
@@ -219,3 +219,36 @@ public:
                                
     } 
 };
+
+
+
+
+
+
+
+
+
+problem 118]  pascals triangle 
+              -----time complexity=o(n*n)
+              -----use of 2D vector
+              
+     
+     class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+    
+        vector <vector <int> > ans(numRows);
+           for(int i=0;i<=numRows-1;i++) 
+           {       
+                   ans[i].resize(i+1);  //it alters the vector by deleting the extra values //also creates position in which elements can be inserted
+                   ans[i][0]=1;
+                   ans[i][i]=1;
+         
+                   for(int j=1;j<i;j++)
+                   {
+                            ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+                   }
+           }
+            return ans;
+    }
+};
